a0d229a4f72d0cff4b5561f13abcc38026ae3eb1
Aaron Jorbin <622599+aaronjorbin@users.noreply.github.com>
2025-02-11 22:30:51 -0400

    Add error handling and write credits to file

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { parse } from 'node-html-parser';
-import { readFileSync } from 'fs';
+import { readFileSync, writeFileSync  } from 'fs';
 import axios from 'axios';
 
 // read the file from the first thing passed to the script and parse it into an array
@@ -31,27 +31,36 @@ for( const profile of data ) {
     if( profiles.has( profile.toLowerCase() ) ) {
         // do nothing
     } else { 
-        const baseUrl = 'https://profiles.wordpress.org/';
-        // get the profile page, urlendcode the profile name
-        const url = baseUrl + encodeURIComponent( profile );
-        // fetch url using axios and throw an error if it is not a 200
-        const response = await axios.get( url ).catch( err => { throw profile; } );
-        // parse the response into an html document
-        const html = parse( response.data );
-        const name = html.querySelector( '.site-header h2' ).text;
-        // create the HTML for the link
-        const link = `<a href="${url}">${name}</a>`;
-        // add the link to the set
-        profiles.set( profile.toLowerCase(), { link, name } );
-        // check if profile is in the credits and if not add it to the newCredits array
-        if( !credits.includes( profile.toLowerCase() ) ) {
-            newCredits.push( profile );
-        }
+		try{
+			const baseUrl = 'https://profiles.wordpress.org/';
+			// get the profile page, urlendcode the profile name
+			const url = baseUrl + encodeURIComponent( profile );
+			// fetch url using axios and throw an error if it is not a 200
+			const response = await axios.get( url ).catch( err => { throw profile; } );
+			// parse the response into an html document
+			const html = parse( response.data );
+			const name = html.querySelector( '.site-header h2' ).text;
+			// create the HTML for the link
+			const link = `<a href="${url}">${name}</a>`;
+			// add the link to the set
+			profiles.set( profile.toLowerCase(), { link, name } );
+			// check if profile is in the credits and if not add it to the newCredits array
+			if( !credits.includes( profile.toLowerCase() ) ) {
+				newCredits.push( profile );
+				props.push(profile.toLowerCase());
+			}
+		} catch ( e ) {
+			console.log( `Error with ${profile}` );
+		}
     }
 };
 
 // take the profiles Map and sort them based on the name
 const sortedProfiles = new Map( [...profiles.entries()].sort( ( a, b ) => a[1].name.localeCompare( b[1].name ) ) );
+
+// sort the credits. 
+newCredits.sort()
+props.sort();
 
 // create the html for the liss and join them with a comma
 const html = [...sortedProfiles.values()].map( p => p.link ).join( ', ' );
@@ -59,4 +68,10 @@ console.log( '-----')
 console.log( html );
 console.log( '-----')
 console.log( 'New Credits:' );
-console.log( newCredits );
+console.log( newCredits.sort() );
+
+// the credids
+const creditsContent = props.map(credit => `'${credit}'`).join(',\n');
+writeFileSync(`${file}.credits.txt`, creditsContent, 'utf8');
+
+console.log( `updated credits array written to ${file}.credits.txt` );
